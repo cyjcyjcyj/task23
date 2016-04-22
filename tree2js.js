78f060b7922aa406da6958bfc058602d7ac8dd06
cyjcyjcyj <510434772@qq.com>
2016-04-22 18:18:44 +0800

    Added files via upload

--- tree2js.js
+++ tree2js.js
@@ -1,4 +1,5 @@
 // JavaScript Document
+
 var queue=[];
 var bfsqueue=[];
 var timer=null; 
@@ -42,34 +43,44 @@ function show(){
 		alltree[i].style.background = '#fff';
 	}
 	i = 0;
-	
-	queue[0].style.background = '#000';
+	//queue[0].style.background = '#000';
 	timer = setInterval(function(){ 
-		i++;
+		//i++;
+
 		if(i >= queue.length){
 			queue[i-1].style.background = '#fff';
 			clearInterval(timer);
 		}else{
-		    if(text.value == queue[i].innerText.split(' ')[0])
-			 queue[i].style.background = '#FF0'
+		    if(text.value && text.value == queue[i].innerHTML.split('<')[0].trim()){
+			   //alert(queue[i].innerText.split(' ')[0]);
+			   //alert(queue[i].innerHTML);
+			   queue[i].style.background = '#FF0'      //元素找到，标注
+			   if(i)
+			     queue[i-1].style.background='#fff';   //恢复父节点的颜色
+			   clearInterval(timer);
+			}
 			else
 			queue[i].style.background = '#000';
 			
-			if(text.value!=queue[i-1].innerText.split(' ')[0])
-			queue[i-1].style.background = '#fff';
+		    //if(text.value!=queue[i-1].innerText.split(' ')[0])    //颜色恢复
+			if(i)
+			 queue[i-1].style.background = '#fff'; 
+			
+			i++;
 		}
+		
 	}, 500);
 }
 
- var text;
+var text;
 window.onload=function(){
-	
 	document.getElementById('dfs').onclick=function(){
 	
 		queue=[];
 		text=document.getElementsByName('find')[0];
 		dfs(document.getElementsByClassName('NO1')[0]);
 		show();
+		//alert(queue[0].innerHTML);
 		
 	}
 	document.getElementById('bfs').onclick=function(){
@@ -79,5 +90,9 @@ window.onload=function(){
 		show();
 	}
 	
-	
 }
+
+
+
+
+
